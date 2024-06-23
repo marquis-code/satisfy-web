114175a4e337d574320e813e99f0b87c8556c4ef
Marquis Abah <marquis.abah@shuttlers.ng>
2024-06-23 21:36:42 +0100

    update implementation for sponsored ads

--- apiFactory/ads.ts
+++ apiFactory/ads.ts
@@ -2,22 +2,22 @@ import { axiosInstance } from "./axios.config";
 
 export const adsApiFactory = {
   $_create_ads(payload: Record<string, any>) {
-    return axiosInstance.post("/sponsored-ads", payload);
+    return axiosInstance.post("/adverts", payload);
   },
 
   $_update_ads(id: string | number, payload: Record<string, any>) {
-    return axiosInstance.patch(`/sponsored-ads/${id}`, payload);
+    return axiosInstance.patch(`/adverts/${id}`, payload);
   },
 
   $_fetch_ads() {
-    return axiosInstance.get("/sponsored-ads");
+    return axiosInstance.get("/adverts");
   },
 
   $_fetch_ads_by_id(id: string | number) {
-    return axiosInstance.get(`/sponsored-ads/${id}`);
+    return axiosInstance.get(`/adverts/${id}`);
   },
 
   $_delete_ads(id: string | number) {
-    return axiosInstance.delete(`/sponsored-ads/${id}`);
+    return axiosInstance.delete(`/adverts/${id}`);
   },
 };
--- composables/sponsored-ads/fetch.ts
+++ composables/sponsored-ads/fetch.ts
@@ -2,17 +2,7 @@ import { adsApiFactory } from "@/apiFactory/ads";
 
 export const useGetAllSponsoredAds = () => {
   const loading = ref(false);
-  const ads = ref([
-    {
-      id: 1,
-      client_name: "Storipod",
-      file_upload:
-        "https://img.freepik.com/free-photo/front-view-young-woman-posing-with-confetti_23-2150402964.jpg?t=st=1718420568~exp=1718424168~hmac=ee731e48c2399579b32467e08b69825e46a7cb55f4213572397ed1661aa6d5ac&w=2000",
-      target_link: "https://www.tfp.is/",
-      start_date: "22-56-2024",
-      end_date: "25-23-2025",
-    },
-  ]) as any;
+  const ads = ref([]) as any;
   const getAllSponsoredAds = async () => {
     loading.value = true;
     try {
