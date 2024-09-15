25cab37bb8493dd2ef30cfa9c7351e0007976be5
Marquis Abah <marquis.abah@shuttlers.ng>
2024-09-15 02:46:47 +0100

    fix bug on storipod create stori

--- components/ads/SponsoredAdsTable.vue
+++ components/ads/SponsoredAdsTable.vue
@@ -70,9 +70,9 @@
           <div class="border-[0.5px] border-gray-50 rounded-lg">
             <dl class="mx-auto grid grid-cols-1 gap-px bg-gray-900/5 sm:grid-cols-2 lg:grid-cols-4 border-[0.5px] border-gray-50 rounded-lg">
                 <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8">
-                  <dt class="text-sm font-medium leading-6 text-gray-500">Total Views</dt>
-                  <dd class="text-xs font-medium text-gray-700">+4.75%</dd>
-                  <dd class="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">100</dd>
+                  <dt class="text-sm font-medium leading-6 text-gray-500">Total Banner Ads</dt>
+                  <dd class="text-xs font-medium text-gray-700"></dd>
+                  <dd class="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">{{adsList.length}}</dd>
                 </div>
                 <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8">
                   <dt class="text-sm font-medium leading-6 text-gray-500">Total Clicks</dt>
